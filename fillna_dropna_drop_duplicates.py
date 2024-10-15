c617717654e7a982915d266afe36d300ff7a44cc
beeravallirama <beeravallirama@gmail.com>
2024-10-15 20:37:58 +0530

    fillna_dropna_drop_dupliates_10_25

--- fillna_dropna_drop_duplicates.py
+++ fillna_dropna_drop_duplicates.py
@@ -21,37 +21,37 @@ df.show()
 
 # fillna() - Filling Missing Values:
 # In PySpark, you can fill missing values with the fillna() method.
-# age_mean = df.select("age").agg({"age": "mean"}).collect()[0][0]
-# df_filled_age = df.fillna({'age':age_mean})
-#
-# df_filled_salary = df_filled_age.fillna({'salary': 0})
-#
-# df_filled_all = df.fillna('unknown')
-#
-# df_filled_salary.show()
+age_mean = df.select("age").agg({"age": "mean"}).collect()[0][0]
+df_filled_age = df.fillna({'age':age_mean})
+
+df_filled_salary = df_filled_age.fillna({'salary': 0})
+
+df_filled_all = df.fillna('unknown')
+
+df_filled_salary.show()
 
 # dropna() - Dropping Missing Values:
 # In PySpark, you can drop rows containing missing values using dropna().
 
-# df_droped_any = df.dropna()
-#
-# df_droped_all = df.dropna(how='all')
-#
-# df_droped_spcific = df.dropna(subset=['age'])
-#
-# df_droped_any.show()
+df_droped_any = df.dropna()
+
+df_droped_all = df.dropna(how='all')
+
+df_droped_spcific = df.dropna(subset=['age'])
+
+df_droped_any.show()
 
 
 # drop_duplicates() - Removing Duplicates:
 # In PySpark, you can remove duplicate rows using dropDuplicates().
 
 
-# df_no_duplicates = df.dropDuplicates()
-#
-# df_no_duplicates_specific = df.dropDuplicates(['name', 'age'])
-#
-# df_no_duplicates_last = df.dropDuplicates(['name', 'age', 'salary'])
-#
-# df_no_duplicates.show()
+df_no_duplicates = df.dropDuplicates()
+
+df_no_duplicates_specific = df.dropDuplicates(['name', 'age'])
+
+df_no_duplicates_last = df.dropDuplicates(['name', 'age', 'salary'])
+
+df_no_duplicates.show()
 
 
